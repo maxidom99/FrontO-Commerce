f73c9f8a9eb1b2a756a66929b35f1420c9ce855b
Lucio Schiavoni <104010031+LucioSchiavoni@users.noreply.github.com>
2024-05-09 15:07:18 -0300

    style(): product card, cart

--- src/components/Admin/Products/ProductItem.jsx
+++ src/components/Admin/Products/ProductItem.jsx
@@ -1,21 +1,53 @@
+import { 
+    Card, 
+    CardBody, 
+    Image,
+    Stack,
+    Heading,
+    Text,
+    Divider,
+    CardFooter,
+    ButtonGroup,
+    Button,
+
+} from "@chakra-ui/react"
+
 export default function ProductItem({product, addToCart}){
 
-    const {id, descripcion, nombres, precios, img_product} = product
+    const { descripcion, nombres, precios, img_product} = product
 
 
     return(
-        <div>
-            <div className="col-4" key={id}>
-            <img src={img_product} alt="img_product" className="img-fluid object-cover" />
-            </div>
-            <div>
-                    <h3>{nombres}</h3>
-                <p>{descripcion}</p>
-                <p>{precios}</p>
-                <button type="button" className="px-3 py-2 rounded-md shadow-xl font-semibold bg-gray-50" onClick={() => addToCart(product)}>
-                    Agregar al Carrito
-                </button>
-            </div>
-        </div>
+       <Card maxW='sm' className="shadow-xl hover:scale-105 transition-all duration-300 delay-150" >
+  <CardBody >
+    <Image
+      src={img_product}
+      alt='img_product'
+      borderRadius='lg' className="w-full border  h-64 object-cover"
+    />
+    <Stack mt='6' spacing='3'>
+      <Heading size='md'>{nombres}</Heading>
+      <Text>
+        {descripcion}
+      </Text>
+      <Text color='zinc.600' fontSize='2xl' className="font-semibold">
+        ${precios}
+      </Text>
+    </Stack>
+  </CardBody>
+  <Divider className="text-zinc-400 "/>
+  <CardFooter>
+    <ButtonGroup >
+      <Button className="">
+        Comprar ahora
+      </Button>
+      <Button  onClick={() => addToCart(product)} >
+        Agregar al carro
+      </Button>
+    </ButtonGroup>
+  </CardFooter>
+</Card>
     )
 }
+
+
--- src/components/Cliente/Cart/Cart.jsx
+++ src/components/Cliente/Cart/Cart.jsx
@@ -6,11 +6,12 @@ import {
   DrawerContent,
   DrawerCloseButton,
   DrawerHeader,
-  DrawerBody
-
+  DrawerBody,
+  Icon
  } from "@chakra-ui/react"
  import { FaCartPlus } from "react-icons/fa";
 import { useMemo } from "react"
+import { DeleteIcon, AddIcon, MinusIcon } from '@chakra-ui/icons'
 
 const Cart = ({cart, removeFromCart, incrementCart, decrementCart, clearCart}) => {
 
@@ -29,9 +30,14 @@ const handleClick = () => {
         <span className="text-center text-2xl">
           {
             !isEmpty ?
-           <>  <div className="flex gap-2"> 
+           <>  <div className="flex gap-2">
             <FaCartPlus />
-           
+               <Icon viewBox='0 0 200 200' color='blue.500' className="text-sm">
+  <path
+    fill='currentColor'
+    d='M 100, 100 m -75, 0 a 75,75 0 1,0 150,0 a 75,75 0 1,0 -150,0'
+  />
+</Icon> 
             <p className="text-sm "></p>
             </div>
            </>
@@ -51,29 +57,29 @@ const handleClick = () => {
           <DrawerHeader className="text-center ">Carrito de compras</DrawerHeader>
           <DrawerBody>
           {isEmpty ? (
-        <p className="text-center mt-5 text-md ">Carrito vacío</p>
+        <p className="text-center mt-5 text-md ">Tu carro esta vacío.</p>
       ) : (
         <>
-        <div className="flex flex-col justify-center items-center">
+        <div className="flex flex-col justify-center  items-center">
 
       
           {cart.map((item) => (
-            <div key={item.id} className="flex  gap-5 items-center w-full mt-2 font-semibold text-md">
+            <div key={item.id} className="flex border  shadow-xl rounded-md p-2   gap-3 items-center  mt-2 font-semibold text-md">
               <img src={item.img_product} className='w-24 h-24 object-cover rounded-md' alt="img_product" />
               <p className="">{item.nombres}</p>
               <p className="text-xl">${item.precios}</p>
-              <div className="flex items-center text-xl">
-                <button type="button" className="text-3xl mb-2" onClick={() => decrementCart(item.id)}>-</button>
-                <p className="h-auto px-3 rounded-md m-auto bg-zinc-200 ">{item.quantity}</p>
-                <button type="button" className="text-3xl mb-2" onClick={() => incrementCart(item.id)}>+</button>
-                <button type="button" className="ml-2  bg-red-700 rounded-md px-3 h-8 text-white font-semibold" onClick={() => removeFromCart(item.id)}>Eliminar</button>
+              <div className="flex items-center text-xl gap-4  justify-between ">
+                <button type="button" className="text-2xl mb-2" onClick={() => decrementCart(item.id)}><MinusIcon/></button>
+                <p className="h-auto px-3 text-2xl rounded-md m-auto bg-zinc-200 ">{item.quantity}</p>
+                <button type="button" className="text-2xl mb-2" onClick={() => incrementCart(item.id)}><AddIcon/></button>
+                <button type="button" className="flex items-center  bg-red-700 rounded-md px-3 h-8 text-sm text-white font-semibold" onClick={() => removeFromCart(item.id)}><DeleteIcon/></button>
               </div>
             </div>
           ))} 
             </div>
            <div>
                 <p className="text-center mt-5 font-semibold text-xl">Total a pagar ${cartTotal}</p>
-                <button type="button" onClick={clearCart} className="px-3 py-2 w-full font-semibold mt-5 rounded-md shadow-md bg-gray-50">Limpiar carrito</button>
+                <button type="button" onClick={clearCart} className="px-3 border py-2 w-full font-semibold mt-5 rounded-md shadow-md bg-gray-50">Limpiar carrito</button>
               </div>
         </>
       )}
